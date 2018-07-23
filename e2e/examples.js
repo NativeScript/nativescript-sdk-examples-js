c6ca5703c49f6eb6b5922cbfd320e266f99bf8ef
SvetoslavTsenov <tsenov.svetoslav@gmail.com>
2018-07-24 00:11:36 +0300

    test: init

---
+++ e2e/examples.js
@@ -0,0 +1,19 @@
+class Example {
+    constructor(baseExampleName, examplesIn) {
+        this.baseExampleName = baseExampleName;
+        this.examplesIn = examplesIn;
+    }
+
+    get baseExample() {
+        return this.baseExampleName;
+    }
+    get examples() {
+        return this.examplesIn;
+    }
+}
+
+exports.examples = {
+    actionBar : new Example("Action Bar", ["Basics", "Code-Behind"]),
+    activityIndicator : new Example("ActivityIndicator", ["Basics", "Code-Behind", "Styling"]),
+    animations : new Example("Animations", ["Animated Properties", "Animating Multiple Views","Chained Animations", "Properties originX and originY"]),
+}
---
+++ e2e/setup.js
@@ -0,0 +1,12 @@
+const nsAppium = require("nativescript-dev-appium");
+
+let driver;
+before("start server", async () => {
+    await nsAppium.startServer();
+    driver = await nsAppium.createDriver();
+});
+
+after("stop server", async () => {
+    //await driver.quit();
+    await nsAppium.stopServer();
+});
