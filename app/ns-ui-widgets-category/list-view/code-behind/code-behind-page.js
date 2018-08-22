c4832dc5a88de5aa4173f5453fbc768b6cb7a2e5
Nikolay Iliev <Nikolay.Iliev@telerik.com>
2018-08-22 15:20:04 +0300

    docs: simplified list-view getting started

--- app/ns-ui-widgets-category/list-view/code-behind/code-behind-page.js
+++ app/ns-ui-widgets-category/list-view/code-behind/code-behind-page.js
@@ -1,36 +1,24 @@
 // >> require-list-view
- const listViewModule = require("tns-core-modules/ui/list-view");
+const listViewModule = require("tns-core-modules/ui/list-view");
 // << require-list-view
 const dialogs = require("tns-core-modules/ui/dialogs");
-const labelModule = require("tns-core-modules/ui/label");
-const observableArrayModule = require("tns-core-modules/data/observable-array");
+const Label = require("tns-core-modules/ui/label").Label;
+const ObservableArray = require("tns-core-modules/data/observable-array").ObservableArray;
 
-const listViewArray = new observableArrayModule.ObservableArray(
-    [
-        {
-            title:"The Da Vinci Code"
-        },
-        {
-            title:"Harry Potter and the Chamber of Secrets"
-        },
-        {
-            title:"The Alchemist"
-        },
-        {
-            title:"The Godfather"
-        },
-        {
-            title:"Goodnight Moon"
-        },
-        {
-            title:"The Hobbit"
-        }
-    ]);
+const listViewArray = new ObservableArray([
+    { title: "The Da Vinci Code" },
+    { title: "Harry Potter and the Chamber of Secrets" },
+    { title: "The Alchemist" },
+    { title: "The Godfather" },
+    { title: "Goodnight Moon" },
+    { title: "The Hobbit" }
+]);
 
 function onNavigatingTo(args) {
     const page = args.object;
-    const container = page.getViewById("container");
     // >> create-list-view-code
+    const container = page.getViewById("container");
+    
     const listView = new listViewModule.ListView();
     listView.className = "list-group";
     listView.items = listViewArray;
@@ -38,23 +26,22 @@ function onNavigatingTo(args) {
     listView.on(listViewModule.ListView.itemLoadingEvent, (args) => {
         if (!args.view) {
             // Create label if it is not already created.
-            args.view = new labelModule.Label();
+            args.view = new Label();
             args.view.className = "list-group-item";
         }
         (args.view).text = listViewArray.getItem(args.index).title;
 
     });
     listView.on(listViewModule.ListView.itemTapEvent, (args) => {
-        // getting the index of the selected item
         const tappedItemIndex = args.index;
-        // getting the view of the selected ListVies's item
-        // let tappedItemView = args.view;
-        dialogs.alert(`Second ListView item tap ${tappedItemIndex}`)
-        .then(() => {
-            console.log("Dialog closed!");
-        });
+        const tappedItemView = args.view;
+        dialogs.alert(`Index: ${tappedItemIndex} View: ${tappedItemView}`)
+            .then(() => {
+                console.log("Dialog closed!");
+            });
     });
-    // << create-list-view-code
+    
     container.addChild(listView);
+    // << create-list-view-code
 }
 exports.onNavigatingTo = onNavigatingTo;
