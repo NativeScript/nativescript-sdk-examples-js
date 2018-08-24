77b52a5b66bcaee54d7c2ef847c9e925dd2ac38c
Nikolay Tsonev <ntsonev@progress.com>
2018-08-24 16:42:55 +0300

    console.dir example

--- app/ns-framework-modules-category/console/basics/basics-page.js
+++ app/ns-framework-modules-category/console/basics/basics-page.js
@@ -1,10 +1,25 @@
 exports.onNavigatedTo = function (args) {
+    let jsonObj = {
+        objProp: "I am Object!", objProp2:
+        {
+            objProp3: "I am Object!",
+            objProp4: "I am Object!"
+        },
+        objProp5:
+        {
+            objProp6: "I am Object!",
+            objProp7: "I am Object!"
+        },
+        objProp8: "I am Object!",
+        objProp9: ["item", "item"]
+    }
     // >> console-common
     console.log("NativeScript Playground!");
     console.log({ objProp: "I am Object!" });
     console.info("NativeScript Rocks!");
     console.warn("Low memory");
     console.error("Uncaught Application Exception");
+    console.dir(jsonObj);
     // << console-common
 
     // >> console-time
