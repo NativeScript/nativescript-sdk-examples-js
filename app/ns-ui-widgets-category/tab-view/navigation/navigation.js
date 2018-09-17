88b9cc17064fd7332725dee97d3be575f4251ccc
Martin Yankov <m.i.yankov@gmail.com>
2018-09-17 16:33:08 +0300

    docs: minor changes to tabview widget article

--- app/ns-ui-widgets-category/tab-view/navigation/navigation.js
+++ app/ns-ui-widgets-category/tab-view/navigation/navigation.js
@@ -15,25 +15,22 @@ function changeTab(args) {
     const tabSelectedIndex = vm.get("tabSelectedIndex");
     if (tabSelectedIndex === 0) {
         vm.set("tabSelectedIndex", 1);
-        vm.set("tabSelectedIndexResult", "Stats Tab (tabSelectedIndex = 1 )");
     } else if (tabSelectedIndex === 1) {
         vm.set("tabSelectedIndex", 2);
-        vm.set("tabSelectedIndexResult", "Settings Tab (tabSelectedIndex = 2 )");
     } else if (tabSelectedIndex === 2) {
         vm.set("tabSelectedIndex", 0);
-        vm.set("tabSelectedIndexResult", "Profile Tab (tabSelectedIndex = 0 )");
     }
 }
 // displaying the old and new TabView selectedIndex
 function onSelectedIndexChanged(args) {
     if (args.oldIndex !== -1) {
-        const tabSelectedIndex = args.object.selectedIndex;
+        const newIndex = args.newIndex;
         const vm = args.object.bindingContext;
-        if (tabSelectedIndex === 0) {
+        if (newIndex === 0) {
             vm.set("tabSelectedIndexResult", "Profile Tab (tabSelectedIndex = 0 )");
-        } else if (tabSelectedIndex === 1) {
+        } else if (newIndex === 1) {
             vm.set("tabSelectedIndexResult", "Stats Tab (tabSelectedIndex = 1 )");
-        } else if (tabSelectedIndex === 2) {
+        } else if (newIndex === 2) {
             vm.set("tabSelectedIndexResult", "Settings Tab (tabSelectedIndex = 2 )");
         }
         dialogs.alert(`Selected index has changed ( Old index: ${args.oldIndex} New index: ${args.newIndex} )`)
