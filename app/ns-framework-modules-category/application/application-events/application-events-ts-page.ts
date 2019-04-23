5f7d9848ca49c6a8def7828124c2e1f11244d7a0
Nikolay Iliev <Nikolay.Iliev@telerik.com>
2019-04-23 10:29:38 +0300

    docs: improving application example

--- app/ns-framework-modules-category/application/application-events/application-events-ts-page.ts
+++ app/ns-framework-modules-category/application/application-events/application-events-ts-page.ts
@@ -39,7 +39,7 @@ export const onNavigatingTo = (navigatedData) => {
 };
 
 export const onGridLoaded = (eventData) => {
-    // >> application-events-ts
+    // >> application-events-launch-ts
     launchListener = (args) => {
         // The root view for this Window on iOS or Activity for Android.
         // If not set a new Frame will be created as a root view in order to maintain backwards compatibility.
@@ -48,47 +48,63 @@ export const onGridLoaded = (eventData) => {
         vm.set("resumeEvent", "The appication was launched!");
     };
     applicationModule.on(applicationModule.launchEvent, launchListener);
+    // << application-events-launch-ts
 
+    // >> application-events-suspend-ts
     suspendListener = (args) => {
         console.log("The appication was suspended!");
         vm.set("suspendEvent", "The appication was suspended!");
     };
     applicationModule.on(applicationModule.suspendEvent, suspendListener);
+    // << application-events-suspend-ts
 
+    // >> application-events-resume-ts
     resumeListener = (args) => {
         console.log("The appication was resumed!");
         vm.set("resumeEvent", "The appication was resumed!");
     };
     applicationModule.on(applicationModule.resumeEvent, resumeListener);
+    // << application-events-resume-ts
 
+    // >> application-events-exit-ts
     exitListener = (args) => {
         console.log("The appication was closed!");
     };
     applicationModule.on(applicationModule.exitEvent, exitListener);
+    // << application-events-exit-ts
 
-    displayedListener = applicationModule.on(applicationModule.displayedEvent, (args) => {
+    // >> application-events-displayed-ts
+    displayedListener = (args) => {
         console.log("NativeScript displayedEvent!");
         vm.set("displayedEvent", "The appication is displayed!");
-    });
+    };
+    applicationModule.on(applicationModule.displayedEvent, displayedListener);
+    // << application-events-displayed-ts
+
+    // >> application-events-low-memory-ts
     lowMemoryListener = (args) => {
         // the instance that has raidsed the event
         console.log("Instance: ", args.object);
     };
     applicationModule.on(applicationModule.lowMemoryEvent, lowMemoryListener);
+    // << application-events-low-memory-ts
 
+    // >> application-events-orientation-ts
     orientationChangedListener = (args) => {
         // orientationChangedEventData.newValue: "portrait" | "landscape" | "unknown"
         console.log("Orientation: ", args.newValue);
         vm.set("orientation", args.newValue);
     };
     applicationModule.on(applicationModule.orientationChangedEvent, orientationChangedListener);
+    // << application-events-orientation-ts
 
+    // >> application-events-error-ts
     uncaughtErrorListener = (args) => {
         // UnhandledErrorEventData.error: NativeScriptError
         console.log("NativeScript Error: ", args.error);
     };
     applicationModule.on(applicationModule.uncaughtErrorEvent, uncaughtErrorListener);
-    // << application-events-ts
+    // << application-events-error-ts
 };
 
 export const onGridUnloaded = () => {
--- app/ns-framework-modules-category/application/application-events/article.md
+++ app/ns-framework-modules-category/application/application-events/article.md
@@ -4,9 +4,40 @@ With the provided application events the user can handle launch, resume, suspend
 related to the screen orientation, uncaught errors and low memory events.
 
 Use the application method `on` to add event listeners.
-<snippet id='application-events'/>
-<snippet id='application-events-ts'/>
 
+### Launch Event
+<snippet id='application-events-launch'/>
+<snippet id='application-events-launch-ts'/>
+
+### Suspend Event
+<snippet id='application-events-suspend'/>
+<snippet id='application-events-suspend-ts'/>
+
+### Resume Event
+<snippet id='application-events-resume'/>
+<snippet id='application-events-resume-ts'/>
+
+### Exit Event
+<snippet id='application-events-exit'/>
+<snippet id='application-events-exit-ts'/>
+
+### Displayed Event
+<snippet id='application-events-displayed'/>
+<snippet id='application-events-displayed-ts'/>
+
+### Low Memory Event
+<snippet id='application-events-low-memory'/>
+<snippet id='application-events-low-memory-ts'/>
+
+### Orientation Changed Event
+<snippet id='application-events-orientation'/>
+<snippet id='application-events-orientation-ts'/>
+
+### Uncaught Error Event
+<snippet id='application-events-error'/>
+<snippet id='application-events-error-ts'/>
+
+### Unsubscribing Event Listener
 Use the application method `off` to remove the registered event listeners.
 <snippet id='application-events-off'/>
 <snippet id='application-events-off-ts'/>
