c9b4674af45f01c9035e8dc8eb6b5c20e68ede19
NickIliev <Nikolay.Iliev@telerik.com>
2019-07-01 14:35:11 +0300

    initial article

---
+++ app/ns-ui-widgets-category/bottom-navigation/bottom-navigation-page.js
@@ -0,0 +1,24 @@
+const ListViewLinksModel = require("../../links-view-model");
+const link = require("../../link");
+const navigationLinks = [
+    new link("Basics", "ns-ui-widgets-category/bottom-navigation/basics/basics-page")
+    // new link("Styling", "ns-ui-widgets-category/tab-view/styling/styling-page"),
+    // new link("Title Icon Fonts", "ns-ui-widgets-category/tab-view/title-icon-fonts/title-icon-fonts-page"),
+    // new link("Code Behind", "ns-ui-widgets-category/tab-view/code-behind/code-behind-page"),
+    // new link("Navigation", "ns-ui-widgets-category/tab-view/navigation/navigation")
+];
+const navigationLinksTsc = [
+    // new link("Code Behind", "ns-ui-widgets-category/tab-view/code-behind/code-behind-ts-page"),
+    // new link("Navigation", "ns-ui-widgets-category/tab-view/navigation/navigation-ts-page")
+];
+function onNavigatingTo(args) {
+    const page = args.object;
+
+    page.bindingContext = new ListViewLinksModel({
+        links: navigationLinks,
+        actionBarTitle: args.context.title,
+        showTypeScriptExamples:false,
+        tsclinks: navigationLinksTsc
+    });
+}
+exports.onNavigatingTo = onNavigatingTo;
---
+++ app/ns-ui-widgets-category/bottom-navigation/end.md
@@ -0,0 +1,7 @@
+**API Reference for the** [BottomNavigation Class](http://docs.nativescript.org/api-reference/classes/_ui_bottom_navigation_.button.html)
+
+**Native Component**
+
+| Android               | iOS      |
+|:----------------------|:---------|
+| [FrameLayout](https://developer.android.com/reference/android/widget/FrameLayout) | [UITabViewController](https://developer.apple.com/documentation/uikit/uitabbarcontroller?language=objc) | 
